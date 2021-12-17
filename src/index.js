1dc286036286f8e69309c692311626b2bb1a68c8
Iker Sastre <iker.sastre@dominion-global.com>
2021-12-17 18:34:12 +0100

    añado template string

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const SendTelegramMsg = require('./controller/SendTelegramMsg')
 
 TelegramConnection.on('message', async (msg) => {
     
-    var tweet_text = msg.from.username + ': ' + msg.text
+    var tweet_text = `${msg.from.username}: ${msg.text}`
     if ( msg.from.last_name ) {
-        var tweet_text = msg.from.first_name + ' ' + msg.from.last_name + ': ' + msg.text
+        var tweet_text = `${msg.from.first_name} ${msg.from.last_name}: ${msg.text}`
     } 
     const PostTwRes = await PostTwit( tweet_text )
-    SendTelegramMsg(msg.from.username + ' ' + 'ha publicado un tweet')
+    SendTelegramMsg(`${msg.from.username} ha publicado un tweet`)
     SendTelegramMsg(PostTwRes, msg.chat.id)
 
 })
